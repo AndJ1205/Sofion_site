7c80af917c136696792ac29b4669d55d4beaf67f
AndJ1205 <144378919+AndJ1205@users.noreply.github.com>
2025-04-07 17:58:18 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./styles/**/*.{js,ts,jsx,tsx}", // <-- jeśli masz np. `globals.css`
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
